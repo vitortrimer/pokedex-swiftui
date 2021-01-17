2e3cfb71e3e9621552cba0bb34e220588ee4748a
Vitor Trimer <vitortrimer@gmail.com>
2021-01-17 11:01:03 -0300

    Initial Commit

---
+++ PokedexSwiftUI/PokedexSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwiftUIApp.swift
+//  PokedexSwiftUI
+//
+//  Created by Vitor Trimer on 17/01/21.
+//
+
+import SwiftUI
+
+@main
+struct PokedexSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
